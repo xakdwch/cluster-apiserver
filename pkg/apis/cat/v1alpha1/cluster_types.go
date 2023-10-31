b81d75468de0a38db69f30492af90980fbf30bbc
wuchao33447 <wuchao33447@hundsun.com>
2023-10-31 18:17:14 +0800

    add cluster type

--- pkg/apis/cat/v1alpha1/cluster_types.go
+++ pkg/apis/cat/v1alpha1/cluster_types.go
@@ -1,4 +1,3 @@
-
 /*
 Copyright 2023.
 
@@ -21,7 +20,7 @@ import (
 	"context"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
- 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/apiserver-runtime/pkg/builder/resource"
@@ -44,7 +43,7 @@ type Cluster struct {
 // ClusterList
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type ClusterList struct {
-	metav1.TypeMeta   `json:",inline"`
+	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 
 	Items []Cluster `json:"items"`
@@ -52,6 +51,48 @@ type ClusterList struct {
 
 // ClusterSpec defines the desired state of Cluster
 type ClusterSpec struct {
+	//集群唯一标识,uuid
+	Id string `json:"id,omitempty"`
+	// k8s版本号
+	Version string `json:"version,omitempty"`
+	// 供应商,默认裸金属
+	Provider string `json:"provider,omitempty"`
+	// 容器网络
+	NetworkType string `json:"networkType,omitempty"`
+	// 支持docker和containerd
+	RuntimeType string `json:"runtimeType,omitempty"`
+	// 支持systemd和cgroupfs
+	CgroupDriver string `json:"cgroupDriver,omitempty"`
+	// docker的数据目录
+	DockerStorageDir string `json:"dockerStorageDir,omitempty"`
+	// containerd的数据目录
+	ContainerdStorageDir string `json:"containerdStorageDir,omitempty"`
+	// flannel网络模式,host-gw或vxlan
+	FlannelBackend string `json:"flannelBackend,omitempty"`
+	// calico网络模式,bgp或ipip
+	CalicoIpv4poolIpip string `json:"calicoIpv4PoolIpip,omitempty"`
+	// pod子网
+	KubePodSubnet string `json:"kubePodSubnet,omitempty"`
+	// service子网
+	KubeServiceSubnet string `json:"kubeServiceSubnet,omitempty"`
+	// LB模式下apiserver的虚IP
+	LbKubeApiserverIp string `json:"lbKubeApiserverIp,omitempty"`
+	// 最大pod数量
+	KubeMaxPods int `json:"kubeMaxPods,omitempty"`
+	// kube-proxy模式,iptables或ipvs
+	KubeProxyMode string `json:"kubeProxyMode,omitempty"`
+	// ingress类型,nginx-ingress或traefik-ingress
+	IngressControllerType string `json:"ingressControllerType,omitempty"`
+	// 架构,amd64或arm64
+	Architectures string `json:"architectures,omitempty"`
+	// 是否开启kubernetes日志审计
+	KubernetesAudit string `json:"kubernetesAudit,omitempty"`
+	// 容器子网
+	DockerSubnet string `json:"dockerSubnet,omitempty"`
+	// 多网卡环境需指定网卡名称,单网卡可不填
+	NetworkInterface string `json:"networkInterface,omitempty"`
+	// 是否安装GPU套件
+	SupportGpu string `json:"supportGpu,omitempty"`
 }
 
 var _ resource.Object = &Cluster{}
@@ -95,8 +136,42 @@ var _ resource.ObjectList = &ClusterList{}
 func (in *ClusterList) GetListMeta() *metav1.ListMeta {
 	return &in.ListMeta
 }
+
 // ClusterStatus defines the observed state of Cluster
 type ClusterStatus struct {
+	// 集群状态
+	Phase string `json:"phase,omitempty"`
+	//// 结果信息
+	//Message string `json:"message,omitempty"`
+	// 集群安装步骤结果信息
+	Conditions []ClusterCondition `json:"conditions,omitempty"`
+	// 集群apiserver地址列表
+	Apiserver []string `json:"apiserver,omitempty"`
+	// 创建时间
+	CreateAt *metav1.Time `json:"createAt,omitempty"`
+	// 更新时间
+	UpdateAt *metav1.Time `json:"updateAt,omitempty"`
+	//// 集群高可用部署时的虚IP和端口等信息
+	//VirtualAddr []ClusterAddress `json:"virtualAddr,omitempty"`
+	//// 访问集群的认证信息
+	//Credential ClusterCredential `json:"credential,omitempty"`
+	// 节点的基本信息,比如内核版本等
+	//Machines []ClusterMachine `json:"machines,omitempty"`
+}
+
+type ClusterCondition struct {
+	// 集群安装对应的安装步骤
+	Type string `json:"type,omitempty"`
+	// 安装步骤的结果状态
+	Status string `json:"status,omitempty"`
+	// 安装步骤的成功或失败原因
+	Reason string `json:"reason,omitempty"`
+	// 安装步骤执行结果的描述信息
+	Message string `json:"message,omitempty"`
+	// 最后状态检测时间
+	LastProbeTime metav1.Time `json:"lastProbeTime,omitempty"`
+	// 最后状态转变时间
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
 }
 
 func (in ClusterStatus) SubResourceName() string {
